029ecf0567773c5fd83b89b4e4f6cb4881b24267
bguz <betoguz7@gmail.com>
2023-01-26 17:50:16 -0600

    fixed a bracket

--- solutions.js
+++ solutions.js
@@ -238,8 +238,7 @@
 
 
 // // Basic Mathematica Operations
-// function basicOp(operation, value1, value2)
-// {
+// function basicOp(operation, value1, value2) {
 //   switch (operation) {
 //       case '+':
 //         return value1 + value2;
