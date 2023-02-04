1f2f3ba846e148fd45c22476e00727e3797ec0d0
bguz <betoguz7@gmail.com>
2023-02-04 10:55:18 -0600

    added 2 6kyu solutions

--- solutions.js
+++ solutions.js
@@ -1408,17 +1408,26 @@
 
 ======================================== */
 
-// Who Likes It?
-function likes(names) {
-    if (names.length === 0) {
-        return `no one likes this`;
-    } else if (names.length === 1) {
-        return `${names[0]} likes this`;
-    } else if (names.length === 2) {
-        return `${names[0]} and ${names[1]} likes this`;
-    } else if (names.length === 3) {
-        return `${names[0]}, ${names[1]} and ${names[2]} like this`;
-    } else {
-        return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
-    }
+// // Who Likes It?
+// function likes(names) {
+//     if (names.length === 0) {
+//         return `no one likes this`;
+//     } else if (names.length === 1) {
+//         return `${names[0]} likes this`;
+//     } else if (names.length === 2) {
+//         return `${names[0]} and ${names[1]} likes this`;
+//     } else if (names.length === 3) {
+//         return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+//     } else {
+//         return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+//     }
+// }
+
+
+
+
+// Create Phone Number (REFACTOR)
+function createPhoneNumber(numbers){
+    return `(${numbers.slice(0,3).join('')}) ${numbers.slice(3,6).join('')}-${numbers.slice(6,10).join('')}`;
 }
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
