5279fa1b50b2f4074a458a396be2d14463143cb4
bguz <betoguz7@gmail.com>
2023-02-08 17:02:30 -0600

    added a 6kyu solution

--- solutions.js
+++ solutions.js
@@ -1616,3 +1616,12 @@
 //     return `(${numbers.slice(0,3).join('')}) ${numbers.slice(3,6).join('')}-${numbers.slice(6,10).join('')}`;
 // }
 // console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+
+
+
+
+// Array.diff
+function arrayDiff(a, b) {
+    return a.filter(num => !b.includes(num));
+}
+console.log(arrayDiff([1,2,3], [1,2]));
