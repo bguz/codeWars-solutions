1343b2149f7f7f07596fafedbc2fb418165bd598
bguz <betoguz7@gmail.com>
2023-02-11 23:00:28 -0600

    more solutions

--- solutions.js
+++ solutions.js
@@ -1127,24 +1127,50 @@
 
 
 
-// Grade Book
-function getGrade (s1, s2, s3) {
-    const avg = (s1 + s2 + s3) / 3;
+// // Grade Book
+// function getGrade (s1, s2, s3) {
+//     const avg = (s1 + s2 + s3) / 3;
 
-    if (avg >= 90 && avg <= 100) {
-        return 'A';
-    } else if (avg >= 80 && avg < 90) {
-        return 'B';
-    } else if (avg >= 70 && avg < 80) {
-        return 'C';
-    } else if (avg >= 60 && avg < 70) {
-        return 'D';
-    } else {
-        return 'F';
-    }
+//     if (avg >= 90 && avg <= 100) {
+//         return 'A';
+//     } else if (avg >= 80 && avg < 90) {
+//         return 'B';
+//     } else if (avg >= 70 && avg < 80) {
+//         return 'C';
+//     } else if (avg >= 60 && avg < 70) {
+//         return 'D';
+//     } else {
+//         return 'F';
+//     }
+// }
+// console.log(getGrade(95,90,93));
+
+
+
+
+// // Get the Mean of an Array
+// function getAverage(marks){
+//     return Math.floor(marks.reduce((sum, curr) => sum + curr, 0) / marks.length);
+// }
+// console.log(getAverage([2,2,2,2]));
+
+
+
+
+// // My Head is at the Wrong End!
+// function fixTheMeerkat(arr) {
+//     return arr.reverse();
+// }
+// console.log(fixTheMeerkat(['tail', 'body', 'head']));
+
+
+
+
+// N-th Power
+function index(array, n){
+    return array.length > n ? array[n] ** n : -1;
 }
-
-console.log(getGrade(95,90,93));
+console.log(index([1, 3, 10, 100],3));
 
 
 
