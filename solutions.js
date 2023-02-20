a7aaa2c652882709ca8de5f48430d3c4478f9978
bguz <betoguz7@gmail.com>
2023-02-20 08:46:58 -0600

    7kyu solution

--- solutions.js
+++ solutions.js
@@ -1924,11 +1924,23 @@
 
 
 
-// Sum of Minimums!
-function sumOfMinimums(arr) {
-    return arr.map(numArr => Math.min(...numArr)).reduce((sum, curr) => sum + curr, 0);
+// // Sum of Minimums!
+// function sumOfMinimums(arr) {
+//     return arr.map(numArr => Math.min(...numArr)).reduce((sum, curr) => sum + curr, 0);
+// }
+// console.log(sumOfMinimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]]));
+
+
+
+
+// Row Weights
+function rowWeights(array){
+    return [
+        array.filter((num, index) => index % 2 === 0).reduce((sum, curr) => sum + curr, 0),
+        array.filter((num, index) => index % 2 !== 0).reduce((sum, curr) => sum + curr, 0)
+    ]
 }
-console.log(sumOfMinimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]]));
+console.log(rowWeights([100,50]));
 
 
 
