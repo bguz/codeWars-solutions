f1effdbfc971bea88c8b0ab123e3ca2a957d4ac1
bguz <betoguz7@gmail.com>
2023-03-11 08:13:11 -0600

    daily solution

--- solutions.js
+++ solutions.js
@@ -2356,6 +2356,32 @@
 
 
 
+// Run-Length Encoding
+let runLengthEncoding = function(str) {
+    if (str.length === 0) return [];
+
+    let finalArr = [];
+    let count = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === str[i + 1]) {
+            count++;
+        } else {
+            count++;
+            finalArr.push([count, str[i]]);
+            count = 0;
+        }
+    }
+    return finalArr;
+}
+console.log(runLengthEncoding('WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW'));
+
+
+
+
+
+
+
 
 
 
