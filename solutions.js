7f667ec60494f10416c955a0b2c1f6c8d5c23dd1
bguz <betoguz7@gmail.com>
2023-05-24 17:30:04 -0500

    daily solution

--- solutions.js
+++ solutions.js
@@ -3070,6 +3070,15 @@
 
 
 
+// We Have Liftoff
+function liftoff(instructions) {
+    return instructions.sort((a,b) => b - a).join(' ') + ' liftoff!';
+}
+console.log(liftoff([2, 8, 10, 9, 1, 3, 4, 7, 6, 5]));
+
+
+
+
 
 
 
