6ee6e2ff8d385cc5419bcc616d01b42a3bd11c64
bguz <betoguz7@gmail.com>
2023-06-04 09:06:46 -0500

    daily solution

--- solutions.js
+++ solutions.js
@@ -3120,6 +3120,15 @@
 
 
 
+// Sort Numbers
+function solution(nums) {
+    if (nums === null || nums.length === 0) return []
+    return nums.sort((a,b) => a - b);
+}
+console.log(solution([1,2,3,10,5]));
+console.log(solution(null));
+
+
 
 
 
