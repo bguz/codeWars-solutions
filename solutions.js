81f4a2805e491a5944b29b6ac2de778c3d6487b0
bguz <betoguz7@gmail.com>
2023-07-11 08:34:49 -0500

    daily solution

--- solutions.js
+++ solutions.js
@@ -1467,6 +1467,15 @@
 
 
 
+// Enumerable Magic #25 - Take the First N Elements
+function take(arr, n) {
+    return arr.slice(0, n);
+}
+console.log(take([0, 1, 2, 3, 5, 8, 13], 3));
+
+
+
+
 
 
 
