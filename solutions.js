c92376849b08cc12f65e292703adbb1d183416d9
bguz <betoguz7@gmail.com>
2023-07-22 23:13:00 -0500

    daily solution

--- solutions.js
+++ solutions.js
@@ -1534,11 +1534,36 @@
 
 
 
-// Convert to Binary
-function toBinary(n){
-    return Number(n.toString(2));
+// // Convert to Binary
+// function toBinary(n){
+//     return Number(n.toString(2));
+// }
+// console.log(toBinary(11));
+
+
+
+
+// Cat Years, Dog Years
+const humanYearsCatYearsDogYears = function(humanYears) {
+    let catYears = 0;
+    let dogYears = 0;
+    
+    for (let i = 1; i <= humanYears; i++) {
+        if (i === 1) {
+            catYears += 15;
+            dogYears += 15;
+        } else if (i === 2) {
+            catYears += 9;
+            dogYears += 9;
+        } else {
+            catYears += 4;
+            dogYears += 5;
+        }
+    }
+
+    return [humanYears, catYears, dogYears];
 }
-console.log(toBinary(11));
+console.log(humanYearsCatYearsDogYears(1));
 
 
 
