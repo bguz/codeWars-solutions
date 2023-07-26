26fa024eef50d929eed8a63139a8faaf747c4711
bguz <betoguz7@gmail.com>
2023-07-26 09:11:38 -0500

    daily solution

--- solutions.js
+++ solutions.js
@@ -1577,6 +1577,23 @@
 
 
 
+// Drink About
+function peopleWithAgeDrink(old) {
+    switch (true) {
+        case old < 14:
+            return 'drink toddy';
+        case old < 18:
+            return 'drink coke';
+        case old < 21:
+            return 'drink beer';
+        default:
+            return 'drink whisky';
+    }
+}
+console.log(peopleWithAgeDrink(13));
+
+
+
 
 
 
