c585969ceb3b1483cfcac921c06260f7436f145a
bguz <betoguz7@gmail.com>
2023-07-29 22:17:19 -0500

    comment

--- solutions.js
+++ solutions.js
@@ -3618,11 +3618,12 @@
 
 
 
-// // Simple Remove Duplicates
-// function solve(arr) {
-//     return Array.from(new Set(arr.reverse())).reverse();
-// }
-// console.log(solve([3,4,4,3,6,3]))
+// Simple Remove Duplicates
+function solve(arr) {
+    return Array.from(new Set(arr.reverse())).reverse();
+}
+console.log(solve([3,4,4,3,6,3]))
+
 
 
 
