eaaf2783876e48cd69415a35101936145bc7153f
bguz <betoguz7@gmail.com>
2023-08-13 19:43:34 -0500

    na

--- solutions.js
+++ solutions.js
@@ -3067,6 +3067,7 @@
 // }
 // console.log(runLengthEncoding('WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW'));
 
+
 // // Replace With Alphabet Position
 // function alphabetPosition(text) {
 //     const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
