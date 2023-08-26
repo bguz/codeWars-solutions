7a28e699192746b9909bca8310c7f546f017f210
bguz <betoguz7@gmail.com>
2023-08-26 09:39:26 -0500

    daily solution

---
+++ 7kyu/how-many-unique-consonants.js
@@ -0,0 +1,30 @@
+// How Many Unique Consonants (REFACTOR)
+
+// Complete the function that counts the number of unique consonants in a string (made up of printable ascii characters).
+
+// Consonants are letters used in English other than "a", "e", "i", "o", "u".
+
+// Remember, your function needs to return the number of unique consonants - disregarding duplicates. For example, if the string passed into the function reads "add", the function should return 1 rather than 2, since "d" is a duplicate.
+
+// Similarly, the function should also disregard duplicate consonants of differing cases. For example, "Dad" passed into the function should return 1 as "d" and "D" are duplicates.
+
+// Examples
+// "add" ==> 1
+// "Dad" ==> 1
+// "aeiou" ==> 0
+// "sillystring" ==> 7
+// "abcdefghijklmnopqrstuvwxyz" ==> 21
+// "Count my unique consonants!!" ==> 7
+
+function countConsonants(str) {
+    const consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
+    const result = [];
+    str
+        .toLowerCase()
+        .split('')
+        .filter(l => consonants.includes(l))
+        .map(l => {
+            if (!result.includes(l)) result.push(l);
+        })
+    return result.length;
+}
--- solutions.js
+++ solutions.js
@@ -3824,19 +3824,38 @@ function rainAmount(mm){
 
 
 
-// Help Bob Count Letters and Digits
-function countLettersAndDigits(input) {
-    let count = 0;
-    input = input.toLowerCase();
-    for (let i = 0; i < input.toLowerCase().length; i++) {
-        if ((input.charCodeAt(i) >= 97 && input.charCodeAt(i) <= 122) || (input.charCodeAt(i) >= 48 && input.charCodeAt(i) <= 57)) {
-            count++
-        }
-    }
-    return count;
+// // Help Bob Count Letters and Digits
+// function countLettersAndDigits(input) {
+//     let count = 0;
+//     input = input.toLowerCase();
+//     for (let i = 0; i < input.toLowerCase().length; i++) {
+//         if ((input.charCodeAt(i) >= 97 && input.charCodeAt(i) <= 122) || (input.charCodeAt(i) >= 48 && input.charCodeAt(i) <= 57)) {
+//             count++
+//         }
+//     }
+//     return count;
+// }
+// console.log(countLettersAndDigits('hel2!lo'));
+// console.log(countLettersAndDigits('12345f%%%t5t&/6'))
+
+
+
+
+// How Many Unique Consonants (NEEDS REFACTORING)
+function countConsonants(str) {
+    const consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
+    const result = [];
+    str
+        .toLowerCase()
+        .split('')
+        .filter(l => consonants.includes(l))
+        .map(l => {
+            if (!result.includes(l)) result.push(l);
+        })
+    return result.length;
 }
-console.log(countLettersAndDigits('hel2!lo'));
-console.log(countLettersAndDigits('12345f%%%t5t&/6'))
+console.log(countConsonants('sillystring'))
+console.log(countConsonants('FN7KO'))
 
 
 
