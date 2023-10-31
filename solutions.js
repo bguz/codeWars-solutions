3c85a6272eb83f0c6603e8798b9692fbea68eb8c
bguz <betoguz7@gmail.com>
2023-10-30 19:11:47 -0500

    daily solution

--- solutions.js
+++ solutions.js
@@ -4264,6 +4264,16 @@ function rainAmount(mm){
 
 
 
+// Predict Your Age
+function predictAge(age1, age2, age3, age4, age5, age6, age7, age8) {
+    const arr = [...arguments];
+    return Math.floor(Math.sqrt(arr.map(a => a * a).reduce((b,c) => b + c)) / 2);
+}
+console.log(predictAge(65, 60, 75, 55, 60, 63, 64, 45));
+
+
+
+
 
 
 /* =====================================
