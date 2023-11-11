f11179b9011ea1f73ea168323f3173b4bf4d4901
bguz <betoguz7@gmail.com>
2023-11-10 19:48:07 -0600

    comment out

--- solutions.js
+++ solutions.js
@@ -4293,15 +4293,15 @@ function rainAmount(mm){
 
 
 
-// esreveR
-reverse = function(array) {
-    const reversed = [];
-    for (let i = array.length - 1; i >= 0; i--) {
-        reversed.push(array[i]);
-    }
-    return reversed;
-}
-console.log(reverse([1, null, 14, "two"]));
+// // esreveR
+// reverse = function(array) {
+//     const reversed = [];
+//     for (let i = array.length - 1; i >= 0; i--) {
+//         reversed.push(array[i]);
+//     }
+//     return reversed;
+// }
+// console.log(reverse([1, null, 14, "two"]));
 
 
 
