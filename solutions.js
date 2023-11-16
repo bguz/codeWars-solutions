41c41c661233c40d693822505bbaf3a275d2191e
bguz <betoguz7@gmail.com>
2023-11-16 08:35:39 -0600

    comment out

--- solutions.js
+++ solutions.js
@@ -4306,16 +4306,16 @@ function rainAmount(mm){
 
 
 
-// Find The Vowels
-function vowelIndices(word) {
-    const vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
-    const result = [];
-    for (let i = 0; i < word.length; i++) {
-        if (vowels.includes(word[i].toLowerCase())) result.push(i + 1);
-    }
-    return result;
-}
-console.log(vowelIndices('fNUy'));
+// // Find The Vowels
+// function vowelIndices(word) {
+//     const vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+//     const result = [];
+//     for (let i = 0; i < word.length; i++) {
+//         if (vowels.includes(word[i].toLowerCase())) result.push(i + 1);
+//     }
+//     return result;
+// }
+// console.log(vowelIndices('fNUy'));
 
 
 
