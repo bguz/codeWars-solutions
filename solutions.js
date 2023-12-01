4f5f248a728ff037bc4f672e8db9194f98c5c9c1
bguz <betoguz7@gmail.com>
2023-11-30 18:29:41 -0600

    comment out

--- solutions.js
+++ solutions.js
@@ -4397,11 +4397,11 @@ function rainAmount(mm){
 
 
 
-// Use map() to Double the Values in an Array 
-function double(array) {
-    return array.map(n => n * 2);
-}
-console.log(double([1,2,3,4,5]));
+// // Use map() to Double the Values in an Array 
+// function double(array) {
+//     return array.map(n => n * 2);
+// }
+// console.log(double([1,2,3,4,5]));
 
 
 
